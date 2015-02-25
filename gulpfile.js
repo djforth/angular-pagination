462c8c6be19b2a970f6be6208e1de1ed0de2eda2
Adrian Stainforth <adrian@morsedigtial.com>
2015-02-25 15:47:35 +0000

    tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -3,5 +3,5 @@ var requireDir = require('require-dir');
 var dir        = requireDir('./gulp_tasks');
 
 gulp.task('libs', ['vendor', "vendor:test"]);
-gulp.task('build', ["app", "npm_build", "console_strip", 'console_strip_npm'])
+gulp.task('build', ["app", "npm_build"])
 gulp.task('main', ["app:watch", "karma"])
--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-'use strict';
-var pagination, paginationCtrl, paginationDir, paginationFilter, paginationSrv;
+var _, pagination;
 
 require('angular');
 
-paginationCtrl = require('./paginationCtrl.coffee');
-
-paginationDir = require('./paginationDir.coffee');
-
-paginationFilter = require('./paginationFilter.coffee');
-
-paginationSrv = require('./paginationSrv.coffee');
+_ = require('lodash');
 
 pagination = angular.module('$pagination', []).run([
   "$templateCache", function(e) {
     return e.put("directives/pagination.html", '<div class="pagination" ng-show="showPagination"><ul><li class="prev previous_page" ng-class="prevPageDisabled()"><a ng-click="prevPage()" rel="previous" title="Previous" href="javascript:"><</a></li><li ng-class="setActive(n)" ng-repeat="n in range()"><a rel="start" ng-click="setPage(n)" href="javascript:">{{n+1}}</a></li><li ng-class="nextPageDisabled()"><a ng-click="nextPage()" rel="next" title="next" href="javascript:">></a></li></ul></div>');
   }
-]).service('PaginationService', paginationSrv).controller('PaginationCtrl', paginationCtrl).directive('pagination', paginationDir).filter("itemsForCurrentPage", paginationFilter);
-
-module.exports = pagination;
-tionService.getPageCount() > 0;
+]).controller('PaginationCtrl', [
+  '$scope', 'PaginationService', function($scope, PaginationService) {
+    $scope.showPagination = false;
+    if (!(_.isUndefined($scope.itemsperpage) || $scope.itemsperpage <= 0)) {
+      PaginationService.setItemsPerPage($scope.itemsperpage);
+    }
+    return $scope.$watch('totalitems', function(totalItems) {
+      if (!_.isUndefined(totalItems)) {
+        PaginationService.setPageCount(totalItems);
+        $scope.showPagination = PaginationService.getPageCount() > 0;
         if (_.isFunction($scope.changed)) {
           return $scope.changed(PaginationService.getCurrentPage(), PaginationService.getPageCount());
         }
